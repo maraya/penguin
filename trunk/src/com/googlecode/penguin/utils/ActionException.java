976afa8210511e194e5d8d5baee3e39d04d01bdd
marayag <marayag@07205126-0114-5dfe-d775-059882da7b07>
2011-02-18 20:37:59 +0000

    405 error in repo! :(

--- trunk/src/com/googlecode/penguin/utils/ActionException.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.penguin.utils;
-
-public class ActionException extends Exception {
-	private static final long serialVersionUID = -7577319121145917590L;
-	
-	public ActionException (String action, String msg) {
-		super("Failed to execute " + action + " action: " + msg);
-	}
-}
